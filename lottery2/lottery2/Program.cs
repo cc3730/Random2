2b22cdbc5317ad5173ac003bb94488bc15b9413b
m305-17\w401 <erica88330@gmail.com>
2018-04-12 17:37:51 +0800

    程式完成

--- lottery2/lottery2/Program.cs
+++ lottery2/lottery2/Program.cs
@@ -10,6 +10,29 @@ namespace lottery2
     {
         static void Main(string[] args)
         {
+
+            // 將所有號碼加入 List 中
+            List<int> members = new List<int>();
+
+            for(int i = 1; i <= 42; i++)
+            {
+                members.Add(i);
+            }
+
+            // 隨機挑出一個印出
+            Random rand = new Random();
+
+            for(int i = 0; i < 6; i++)
+            {
+                int r = rand.Next(1, members.Count);
+                Console.Write(members[r] + " ");
+                
+                // 並從 List 中刪除
+                members.RemoveAt(r);
+            }
+            
+            // 暫停畫面
+            Console.ReadKey();
         }
     }
 }
